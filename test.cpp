f361930b691885f4b59417e20f3b28c0a0cd7130
Shaoyi Lu <111807151+ShaoyiLu@users.noreply.github.com>
2024-12-30 02:16:36 -0500

    Update test.cpp
    Add doxygen comments

--- test.cpp
+++ test.cpp
@@ -3,34 +3,42 @@
 
 #include "bigint.hpp"
 
-// constructor tester
+/**
+ * @brief Tests the constructors of the BigInt class.
+ * 
+ * This function tests various constructors of the BigInt class, including default constructor, 
+ * sign integer conversion, and string conversion. It also verifies proper handling of invalid inputs.
+ */
 void testConstructor() {
     BigInt zero;
     assert(zero == BigInt(0));
     
-    BigInt longLong(9025467891111682738LL);
+    BigInt longLong(9025467891111682738);
     assert(longLong == BigInt("9025467891111682738"));
     
-    BigInt negativeLongLong(-7762836615529837640LL);
+    BigInt negativeLongLong(-7762836615529837640);
     assert(negativeLongLong == BigInt("-7762836615529837640"));
         
-    // string
     try {
         BigInt invalid("");
         assert(false);
     } catch (const std::invalid_argument&) {
-        // if invalid, pass
     }
     
     try {
         BigInt invalid("89i1o4");
         assert(false);
     } catch (const std::invalid_argument&) {
-        // if invalid, passs
     }
 }
 
-// bigint calculator tester!
+/**
+ * @brief Tests the computation and comparison operators of the BigInt class.
+ * 
+ * This function tests almost all computation operators (+, -, *), unary negation,
+ * and comparison operators (==, !=, <, >, <=, >=). It also validates the functionality 
+ * of pre-increment, post-increment, pre-decrement, and post-decrement operators.
+ */
 void testOperator() {
     BigInt a("13206478842272655311"); 
     BigInt b("80250025245863872589"); 
@@ -38,19 +46,15 @@ void testOperator() {
     BigInt d("-30477676548372141302");
     BigInt o;
 
-    // +
     assert(a + b == BigInt("93456504088136527900"));
     assert(a + o == BigInt("13206478842272655311"));
     
-    // -
     assert(a - d == BigInt("43684155390644796613"));
     assert(o - c == BigInt("48084066885301367633"));
     
-    // *
     assert(c * d == BigInt("1465490637660506965476761506497325278166"));
     assert(o * (a + c) == BigInt("0"));
 
-    // unary -
     BigInt H("90000000000000000000000000000");
     BigInt h("-90000000000000000000000000000");
     assert(-H == BigInt("-90000000000000000000000000000"));
@@ -62,48 +66,38 @@ void testOperator() {
     BigInt R("111888000555222777999333666");
     BigInt Z("333666999222777555000888111");
 
-    // ==
     assert(O == Z);
     assert(!(P == Z));
 
-    // !=
     assert(P != O);
     assert(O != (-Z));
     
-    // <
     assert(Q < O);
     assert((P + R) < Q);
     
-    // >
     assert(R > P);
     assert((Q * P) > Z);
-    
-    // <= 
+
     assert(Z <= O);
     assert((P - Q) <= Z);
-    
-    // >=
+
     assert(R >= Q);
     assert(-Z >= P);
 
     BigInt last1("100010001000100010001000");
     BigInt last2("-888888888855555555553");
     
-    // pre-increment
     assert(++last1 == BigInt("100010001000100010001001"));
     assert(++last2 == BigInt("-888888888855555555552"));
-    
-    // post-increment
+
     assert(last1++ == BigInt("100010001000100010001001"));
     assert(last1 == BigInt("100010001000100010001002"));
     assert(last2++ == BigInt("-888888888855555555552"));
     assert(last2 == BigInt("-888888888855555555551"));
     
-    // pre-decrement
     assert(--last1 == BigInt("100010001000100010001001"));
     assert(--last2 == BigInt("-888888888855555555552"));
     
-    // post-decrement
     assert(last1-- == BigInt("100010001000100010001001"));
     assert(last1 == BigInt("100010001000100010001000"));
     assert(last2-- == BigInt("-888888888855555555552"));
@@ -111,6 +105,14 @@ void testOperator() {
 
 }
 
+/**
+ * @brief The main function for testing the BigInt class.
+ * 
+ * Runs the constructor tests and operator tests for the BigInt class.
+ * Prints the results of each test case and a final success message.
+ * 
+ * @return Returns 0 after all tests complete successfully.
+ */
 int main() {
     testConstructor();
     std::cout << "Pass testConstructor()\n";
